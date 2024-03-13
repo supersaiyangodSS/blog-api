a6bda68b143190171202dca013c1cdb0e9befb31
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-13 13:52:54 +0530

    ⚙️update app to TS

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import { config } from 'dotenv'
+import express, { Request, Response } from 'express';
+import session, { SessionOptions } from 'express-session';
+import connectDB from './config/database.js';
+import usersRoute from './api/users.js'
+import postsRoute from './api/posts.js'
+import { resolve, join } from 'path';
+
+config();
+connectDB()
+
+const app = express()
+
+const oneDay = 1000 * 60 * 60;
+const publicPath = resolve('publish') //TODO: NONE
+const expSession: SessionOptions = {
+  secret: process.env.SESSION_SECRET || 'asjkcfb89343857qf4hvbt78237g5485fi9b5f893437485g7fb7',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: oneDay
+  }
+}
+
+app.use(express.static('public'))
+app.use(session(expSession))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(join('public', 'index.html') ,)//FIX ROU
+})
+
+app.use('/post', postsRoute)
+app.use('/user', usersRoute)
+
+export default app;
