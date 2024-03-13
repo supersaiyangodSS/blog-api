b4f6083eeb0deedc04f0301da35da7404842e6c5
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-13 17:13:00 +0530

    ➕ add Type interface for Posts

---
+++ src/interfaces/PostInterface.ts
@@ -0,0 +1,22 @@
+import { Document } from "mongoose";
+
+export interface IPost extends Document {
+    title: string,
+    content: string,
+    author: string,
+    tags: string[],
+    createdAt: Date,
+    updatedAt: Date,
+    publishedAt: Date,
+    isPublished: boolean,
+    views: number,
+    likes: number[],
+    likedBy: string[],
+    comments: Array<{
+        text: string,
+        role: string,
+        createdTime: Date,
+        userId: string,
+        editedAt: Date
+    }>
+}
